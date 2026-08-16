ff672d2b82be323f3118d8aefec86758bb549841
Ronak Jawalia <ronakjawaliya@gmail.com>
2026-08-16 22:48:02 +0530

    Add GET expenses endpoint

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+expenses = []
+
 @app.route('/')
 def home():
     return "Expense Budget API is running!"
@@ -9,4 +11,14 @@ def home():
 @app.route('/expenses', methods=['POST'])
 def add_expense():
     data = request.json
+    expenses.append(data)
     return data
+
+@app.route('/expenses', methods=['GET'])
+def get_expenses():
+    return expenses
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
