e37dc419823035e164ff342dae258c1c633154b9
Ronak Jawalia <ronakjawaliya@gmail.com>
2026-08-17 23:37:57 +0530

    Add delete expense endpoint

--- app.py
+++ app.py
@@ -10,14 +10,25 @@ def home():
 
 @app.route('/expenses', methods=['POST'])
 def add_expense():
-    data = request.json
+    data = request.get_json()
+    data["transaction_id"] = len(expenses) + 1
     expenses.append(data)
     return data
+
 
 @app.route('/expenses', methods=['GET'])
 def get_expenses():
     return expenses
 
+
+@app.route('/expenses/<transaction_id>', methods=['DELETE'])
+def delete_expense(transaction_id):
+    for expense in expenses:
+        if expense["transaction_id"] == int(transaction_id):
+            expenses.remove(expense)
+            return {"message": "Expense deleted successfully!"}
+
+    return {"message": "Expense not found."}, 404
 
 
 if __name__ == "__main__":
